567dcf8ae663f66ea0d41225f4f156ebd02a894f
saroar <saroar9@gmail.com>
2020-08-26 18:05:45 +0300

    push app code

---
+++ BugReport/BugReportApp.swift
@@ -0,0 +1,17 @@
+//
+//  BugReportApp.swift
+//  BugReport
+//
+//  Created by Saroar Khandoker on 26.08.2020.
+//
+
+import SwiftUI
+
+@main
+struct BugReportApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BugReport/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BugReport
+//
+//  Created by Saroar Khandoker on 26.08.2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
